cf898e54279b440fd3bc2c92de1c60e4ff0db0e3
kamilchulakov <mr1english4@gmail.com>
2023-03-20 00:36:17 +0300

    lab1: refactor

--- src/test/kotlin/SortTest.kt
+++ src/test/kotlin/SortTest.kt
@@ -1,6 +1,5 @@
-import org.junit.jupiter.api.Assertions.*
-
 import Sort.selectionSort
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class SelectionSortTest {
@@ -8,7 +7,7 @@ class SelectionSortTest {
     fun `simple sort`() {
         val a = arrayOf(5, 42, 3, 0)
         val logs = selectionSort(a)
-        assertArrayEquals(a, arrayOf(0, 3, 5, 42))
-        assertArrayEquals(logs.toTypedArray(), arrayOf(0, 5, 3, 42, 5, 42))
+        assertThat(a).isEqualTo(arrayOf(0, 3, 5, 42))
+        assertThat(logs).isEqualTo(listOf(0, 5, 3, 42, 5, 42))
     }
 }
