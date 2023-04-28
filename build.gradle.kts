adfc2ccefc5195ffbbe0602b2b1bb9277a1a9711
kamilchulakov <mr1english4@gmail.com>
2023-04-29 01:46:17 +0300

    kinda works...

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,17 +13,17 @@ repositories {
 }
 
 dependencies {
-    testCompileOnly("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
     testCompileOnly("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
     testImplementation("org.assertj:assertj-core:3.24.2")
 }
 
 tasks.test {
-    exclude("**/*TestInputProvider.class")
+//    exclude("**/*TestInputProvider.class")
     useJUnitPlatform()
-    finalizedBy(tasks.jacocoTestReport)
-    finalizedBy(tasks.jacocoTestCoverageVerification)
+//    finalizedBy(tasks.jacocoTestReport)
+//    finalizedBy(tasks.jacocoTestCoverageVerification)
 }
 
 tasks.jacocoTestReport {
--- src/test/kotlin/MathTest.kt
+++ src/test/kotlin/MathTest.kt
@@ -1,19 +1,19 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.Offset.offset
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ArgumentsSource
-import org.junit.jupiter.params.provider.ArgumentsProvider
 import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 import kotlin.math.PI
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathTest {
     @Test
     fun arctg() {
-        assertThat(Math.arctg(1.0, 0.001))
-            .isCloseTo(PI/4, offset(0.001))
+        assertThat(Math.arctg(1.0, 0.00001))
+            .isCloseTo(PI/4, offset(0.00001))
     }
 
     @Test
@@ -23,7 +23,7 @@ class MathTest {
     }
 
     @ParameterizedTest
-    @ArgumentsSource(TestInputProvider::class)
+    @MethodSource("provideArguments")
     fun parameterizedTest(x: Double, eps: Double, expected: Double) {
         assertThat(Math.arctg(x, eps))
             .isCloseTo(expected, offset(eps))
@@ -31,14 +31,12 @@ class MathTest {
             .isCloseTo(-expected, offset(eps))
     }
 
-    class TestInputProvider : ArgumentsProvider {
-        override fun provideArguments(context: ExtensionContext?): Stream<Arguments> =
-            Stream.of(
-                Arguments.of( 0.0,       0.00001,    0.0),
-                Arguments.of( 0.57735,   0.00001,    0.523598),
-                Arguments.of( 1.73205,   0.00001,    1.047197),
-                Arguments.of( 100,       0.00001,    1.560796),
-                Arguments.of( 10000,     0.00001,    1.570696),
-            )
-    }
+    private fun provideArguments(): Stream<Arguments> =
+        Stream.of(
+            Arguments.of(0.0,       0.00001,    0.0),
+            Arguments.of(0.57735,   0.00001,    0.523598),
+            Arguments.of(1.73205,   0.00001,    1.047197),
+            Arguments.of(100.0,     0.00001,    1.560796),
+            Arguments.of(10000.0,   0.00001,    1.570696),
+        )
 }
