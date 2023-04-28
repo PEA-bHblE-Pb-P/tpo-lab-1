8f3d7559b03cb188b9f84f142efb05c2d1aa493f
Roman <roman.deev06@gmail.com>
2023-04-29 02:44:02 +0300

    fix more one args

--- src/main/kotlin/Math.kt
+++ src/main/kotlin/Math.kt
@@ -1,16 +1,34 @@
 import kotlin.math.abs
 import kotlin.math.pow
+import kotlin.math.PI
 
 object Math {
     fun arctg(x: Double, eps: Double = 0.001): Double {
+        var normalizedX = x
+        var moreOne = false
+        var sign = 1
+        if (normalizedX < 0){
+            normalizedX *= -1
+            sign = -1
+        }
+        if (abs(normalizedX) > 1) {
+            normalizedX = 1 / normalizedX
+            moreOne = true
+        }
+
         var result = 0.0
         var cur = eps
         var i = 0
         while (eps <= abs(cur)) {
-            cur = (-1.0).pow(i) * x.pow(2 * i + 1) / (2 * i + 1)
+            cur = (-1.0).pow(i) * normalizedX.pow(2 * i + 1) / (2 * i + 1)
             result += cur
             i += 1
         }
-        return result
+
+        return if (moreOne) {
+            sign * (PI / 2 - result)
+        } else {
+            sign * result
+        }
     }
 }
